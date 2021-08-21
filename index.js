6ca727764770137c6918e3f7b181d48cbfa4e78e
Billy Leitch <williaml@equitise.com>
2021-08-21 17:38:03 +1000

    Finished unit testing, final debugging and README documentation

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const readline = require('readline');
-const { calculateDaysDifference, validateDateStr } = require('./helpers');
-// What about the different ways to enter a date - what assumptions are we making around which format the user will enter the date
+const { differenceDays, validateDateStr } = require('./helpers');
 
 // Node command line interface application to read two dates from standard input and calculate the number days between them
 // The goal of this application is to guide CLI user to enter two dates, check if these are valid and then calculate and return the difference in days
 // Assumptions:
-//  -> User provides two dates strings in the format DD/MM/YYYY (no time component) (TODO reword)
+//  -> User provides two dates strings in the format DD/MM/YYYY
 
-
-// Get input from the user
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -24,9 +21,12 @@ const getDateFromUser = (msg, onValidDateEntered) =>
     return onValidDateEntered(dateStr);
   });
 
+console.log('This is program to calculate the number of whole days between two dates (excluding the from and to days). \n');
+console.log('You will be prompted to enter the from and to dates seperately. Please enter each dates in the format DD/MM/YYYY (e.g. 01/01/2021 for the 1st of January 2021) and confirm your input by hitting the enter key. \n');
 getDateFromUser('Please enter the first date: ', dateStrOne => {
   getDateFromUser('Please enter the second date: ', dateStrTwo => {
-    console.log(`There are ${calculateDaysDifference(dateStrOne, dateStrTwo)} days between ${dateStrOne} and ${dateStrTwo}.`);
+    const daysDiff = differenceDays(dateStrOne, dateStrTwo);
+    console.log(`There ${daysDiff === 0 && 'no whole days' || daysDiff === 1 && 'is 1 whole day' || `are ${daysDiff} whole days`} between ${dateStrOne} and ${dateStrTwo}.`);
     return rl.close();
   })
 });
--- tests.js
+++ tests.js
@@ -1,5 +1,5 @@
-const { calculateDaysDifference, validateDateStr } = require('./helpers');
-// Unit testing
+const { BAD_FORMAT_MSG, OUTSIDE_DTE_RNG_MSG } = require("./constants");
+const { differenceDays, validateDateStr } = require('./helpers');
 
 function Test(input, expected, result) {
   this.input = input;
@@ -7,21 +7,24 @@ function Test(input, expected, result) {
   this.result = result;
 }
 
-// Testing calculateDayDifference helper
+// Testing validation helper
+console.log('Testing validateDateStr helper \n');
+console.table(new Test("Invalid date format: 1991/01/23", BAD_FORMAT_MSG, validateDateStr('1991/01/23')));
+console.table(new Test("Valid date: 21/08/2021", '', validateDateStr('21/08/2021')));
+console.table(new Test("Valid date: 01/01/2001", '', validateDateStr('01/01/2001')));
+console.table(new Test("Valid date: 03/01/2001", '', validateDateStr('03/01/2001')));
+console.table(new Test("Valid date: 2/6/1963", '', validateDateStr('2/6/1963')));
+console.table(new Test("Valid date: 22/6/1983", '', validateDateStr('22/6/1983')));
+console.table(new Test("Valid date: 4/7/1984", '', validateDateStr('4/7/1984')));
+console.table(new Test("Valid date: 25/12/1984", '', validateDateStr('25/12/1984')));
+console.table(new Test("Valid date: 3/1/1989", '', validateDateStr('3/1/1989')));
+console.table(new Test("Valid date: 3/8/1983", '', validateDateStr('3/8/1983')));
+console.table(new Test("Outside date range: 01/01/1899", OUTSIDE_DTE_RNG_MSG, validateDateStr('01/01/1899')));
+console.table(new Test("Outside date range: 01/01/3000", OUTSIDE_DTE_RNG_MSG, validateDateStr('01/01/3000')));
+
+// Testing differenceDays helper
 console.log("Testing calculateDaysDifference helper \n");
-console.table(new Test("01/01/2001 to 03/01/2001", 1, calculateDaysDifference("01/01/2001", "03/01/2001")));
-console.table(new Test("2/6/1963 to 22/6/1983", 19, calculateDaysDifference("2/6/1983", "22/6/1983")));
-console.table(new Test("4/7/1984 to 25/12/1984", 173, calculateDaysDifference("4/7/1984", "25/12/1984")));
-console.table(new Test("3/1/1989 to 3/8/1983", 2036, calculateDaysDifference("3/1/1989", "3/8/1983")));
-
-// Check invalid date throws error
-// Check the format of the date string matches DD/MM/YYYY
-// Check the date is valid  
-
-// Calulate the difference in days
-
-
-// test calculateDaysDifference helper
-//a) 2/6/1983 to 22/6/1983 19 days
-//b) 4/7/1984 to 25/12/1984 173 days
-//c) 3/1/1989 to 3/8/1983 2036 days
+console.table(new Test("01/01/2001 to 03/01/2001", 1, differenceDays("01/01/2001", "03/01/2001")));
+console.table(new Test("2/6/1983 to 22/6/1983", 19, differenceDays("2/6/1983", "22/6/1983")));
+console.table(new Test("4/7/1984 to 25/12/1984", 173, differenceDays("4/7/1984", "25/12/1984")));
+console.table(new Test("3/1/1989 to 3/8/1983", 2036, differenceDays("3/1/1989", "3/8/1983")));
